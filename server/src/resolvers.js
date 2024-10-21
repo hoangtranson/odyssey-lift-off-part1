cd19a151c37e426ba09fa45e16788a54ae7ea3e7
hoangtranson <son.hoang01@gmail.com>
2024-10-21 18:08:30 +0700

    resolver chain

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,10 +5,25 @@ const resolvers = {
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
         },
+        track: async (_, {id}, {dataSources}) => {
+            // get track details
+            console.log('track resolver id:', id);
+            return await dataSources.trackAPI.getTrack(id);
+
+            // get module details for the track
+            // const modules = await dataSources.trackAPI.getTrackModules(id);
+
+            // shape the data in the way that the schema expects it
+            // return { ...track, modules };
+        },
     },
     Track: {
-        author: async (parent, _, { dataSources }) => {
-            return dataSources.trackAPI.getAuthor(parent.authorId);
+        author: async ({authorId}, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+        modules: async ({id}, _, {dataSources}) => {
+            console.log('track modules resolver id:', id);
+            return dataSources.trackAPI.getTrackModules(id);
         },
     },
 };
